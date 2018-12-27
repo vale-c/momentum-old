5b9103f97604281035a711d1de602c1c7f6a78dd
Vale <vale@MBP-di-Vale-001.homenet.telecomitalia.it>
2018-12-27 15:16:11 +0100

    changed API to OWM and fixed weather icon rendering

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -2,23 +2,28 @@ import axios from 'axios';
 import React from 'react';
 import './Weather.css';
 
-//const GOOGLE_API_KEY = "AIzaSyAOcD54Wg5Tsi9WLR0DDiqZ8kLWONvXwuY";
 const CORS_HEADER = "https://cors-anywhere.herokuapp.com/";
-const DARKSKY_API_CALL = "https://api.darksky.net/forecast/640bddbf1aa37eddd8253e47a46a46a8/";
+//const DARKSKY_API_CALL = "https://api.darksky.net/forecast/640bddbf1aa37eddd8253e47a46a46a8/";
 
 class Weather extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      temperature: "",
-      summary: "",
+      city: "",
+      temp: "",
+      description: "",
       precipIntensity: "",
       precipProbability: "",
       windSpeed: ""
     };
+    this.getWeatherData = this.getWeatherData.bind(this);
   }
 
-  componentDidMount() {
+  componentWillMount() {
+    this.getLocation();
+  }
+
+  getLocation = () => {
       axios
         .get("https://api.ipdata.co/?api-key=036623005d047344581751a318e8869d09652e6c031e7d2be99e7e74")
         .then(response => {
@@ -26,8 +31,8 @@ class Weather extends React.Component {
             city: response.data.city,
             region: response.data.region,
             country_name: response.data.country_name,
-            latitude: response.data.latitude,
-            longitude: response.data.longitude,
+            latitude: response.data.latitude, //not actually displayed on screen
+            longitude: response.data.longitude, //not actually displayed on screen
             emoji_flag: response.data.emoji_flag
           });
         })
@@ -36,69 +41,59 @@ class Weather extends React.Component {
         });
   };
 
+  componentWillReceiveProps(nextProps) {
+    this.getWeatherData();
+  }
+
   getWeatherData = () => {
     axios
-      .get(CORS_HEADER + DARKSKY_API_CALL + this.state.latitude + "," + this.state.longitude + "?units=si")
+      // Old Darksy API CALL
+      // .get(CORS_HEADER + DARKSKY_API_CALL + this.state.latitude + "," + this.state.longitude + "?units=si")
+      .get(CORS_HEADER +"https://api.openweathermap.org/data/2.5/weather?lat="+ this.state.latitude + "&lon="+this.state.longitude+"&units=metric&appid=13b0886c7c035390785605fc1c637712")
       .then(response => {
         this.setState({
-          temperature: response.data.currently.temperature,
-          summary: response.data.currently.summary,
-          icon: response.data.currently.icon,
-          precipIntensity: response.data.currently.precipIntensity,
-          precipProbability: response.data.currently.precipProbability,
-          windSpeed: response.data.currently.windSpeed
+          temp: response.data.main.temp,
+          description: response.data.weather[0].main,
+          id: response.data.weather[0].id,
+          wind: response.data.wind.speed //not actually displayed on screen
         });
       })
       .catch(error => {
         console.log(error);
       });
-     console.log("Temperature: " + this.state.temperature);
-     console.log("Summary: " + this.state.summary);
-     console.log("Precipitation Intensity: " + this.state.precipIntensity);
-     console.log("Precipitation Probability: " + this.state.precipProbability);
-     console.log("Wind Speed: " + this.state.windSpeed);
+     console.log("Temperature: " + this.state.temp);
+     console.log("Description: " + this.state.description);
+     console.log("ID: " + this.state.id);
+     console.log("Wind Speed: " + this.state.wind);
      //get lat & long values from previous API call
      console.log("Latitude: " + this.state.latitude);
      console.log("Longitude: " + this.state.longitude);
   };
 
+ 
   render() {
-    const { city, region, country_name, emoji_flag, temperature, summary, icon } = this.state;
 
-    const WeatherData = ({ city, region, country_name, emoji_flag, temperature, summary, icon }) => <div>
-        <p className="location">
-          {city},{region}
-        </p>
-        <p className="country_and_flag">
-          {country_name} {emoji_flag}
-        </p>
-        <p className="temp"> {temperature} </p>
-        <p className="summary"> {summary} </p>
-        <i className="wi">{icon}</i>
+    let hr = new Date().getHours();
+    let tod = hr >= 17 ? "night" : "day";
+    
+    const { city, region, country_name, emoji_flag, temp, description, id } = this.state;
+
+    const WeatherData = ({ city, region, country_name, emoji_flag, temp, description, id }) => 
+      <div>
+          <p className="location"> {city}, {region} </p>
+          <p className="country_and_flag">  {country_name} {emoji_flag} </p>
+    
+          <p className="temp-desc"> {temp} °, {description} </p>
+          <i id='icon' className={'wi wi-owm-' + tod + '-' + id}></i>
       </div>;
-
-    const Button = ({ onClick }) => (
-      <button type="button" className="weather-btn" onClick={onClick}>
-        Get Weather Data
-      </button>
-    );
-
-    return (
-      <div className="container">
-        <div className="weather-wrapper">
-          <Button onClick={this.getWeatherData} />
-          <WeatherData
-            city={city}
-            region={region}
-            country_name={country_name}
-            emoji_flag={emoji_flag}
-            temperature={temperature}
-            summary={summary}
-            icon={icon}
-          />
+    
+    return <div className="container">
+        <div className="weatherWrapper" onClick={this.getWeatherData}>
+          <WeatherData city={city} region={region} country_name={country_name} emoji_flag={emoji_flag} 
+            /* from OpenWeatherMap API CALL */
+            temp={temp} description={description} id={id} />
         </div>
-      </div>
-    );
+      </div>;
   }
 }
 
