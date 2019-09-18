69c53c9238c4940497bcf3869a07ef2f40954745
unknown <valent95@gmail.com>
2019-09-18 12:19:46 +0200

    latest changes to WeatherCard

--- src/components/Weather/WeatherCard.js
+++ src/components/Weather/WeatherCard.js
@@ -19,9 +19,8 @@ class Card extends React.Component {
             <h3 className="weekDay">{moment(newDate).format('dddd')}</h3>
             <p className="currentDate">{moment(newDate).format('MMMM Do')}</p>
             <i id="icon" className={imgURL}></i>
-            <p className="minMax">
-              <strong>Max</strong> {Math.round(this.props.day.main.temp_max)} °C / 
-              <strong>Min</strong> {Math.round(this.props.day.main.temp_min)}°C</p>
+            <p className="min"><strong>Min:</strong> {Math.round(this.props.day.main.temp_min)}°C</p>
+            <p className="max"><strong>Max:</strong> {Math.round(this.props.day.main.temp_max)}°C</p>
             <h4 className="card-text">{this.props.day.weather[0].description}</h4>
       </div>
     )
